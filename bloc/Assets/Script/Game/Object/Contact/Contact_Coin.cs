4159a651bf7882d9ad2298cc42cb02cb1f592ec8
Taisei-Kurihara <163286893+Taisei-Kurihara@users.noreply.github.com>
2025-10-13 12:10:27 +0900

    移動制御安定 => 壁際 (90度) で少し安定しない

--- bloc/Assets/Script/Game/Object/Contact/Contact_Coin.cs
+++ bloc/Assets/Script/Game/Object/Contact/Contact_Coin.cs
@@ -7,7 +7,7 @@ public class Contact_Coin : Contact_abstract
     override protected void Contact(Collider2D collider)
     {
         // コインを取得したときの処理
-        Debug.Log("Coin collected!");
+        //Debug.Log("Coin collected!");
         Score.Instance().AddCoin(coins); // スコアを増やす
         AudioManager.Instance().PlayOneShot(contactSound); // 効果音を再生
         Destroy(gameObject); // コインオブジェクトを削除
